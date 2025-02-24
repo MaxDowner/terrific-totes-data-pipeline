2f9add0022dc6d3b6a6afd34b0210af371578078
CharlotteHooson <CharlotteHooson@outlook.com>
2025-02-24 15:36:29 +0000

    create iam.tf and create an iam trust policy and a lambda role

---
+++ terraform/iam.tf
@@ -0,0 +1,17 @@
+# Define
+data "aws_iam_policy_document" "trust_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+# Create
+resource "aws_iam_role" "lambda_role" {
+  name_prefix        = "role-${var.lambda_name}"
+  assume_role_policy = data.aws_iam_policy_document.trust_policy.json
+}
