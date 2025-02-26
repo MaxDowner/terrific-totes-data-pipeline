2dae7468d369e90da64f043ad37be9905b59e4e6
Morgan Lamb <morgan.lamb@hotmail.com>
2025-02-26 11:01:34 +0000

    Create ingestion_lambda and test file with functions to be added

---
+++ src/ingestion_lambda.py
@@ -0,0 +1,9 @@
+from src.util.ingress import ingress_handler
+
+def ingestion_lambda_handler(event, context):
+    pass
+    # Retrieve all data into data dumps list
+    # ingress_handler
+    # export data into s3 bucket
+    
+    
