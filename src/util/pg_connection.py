329e22e652a81b46c11411f7594d70f118129958
AndrewFudge <fudge@gmx.co.uk>
2025-02-26 09:32:28 +0000

    ran black and Flake8 to set formatting

--- src/util/pg_connection.py
+++ src/util/pg_connection.py
@@ -6,16 +6,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def connect_to_db():
     return Connection(
-        user = os.getenv("PG_USER"),
-        password = str(os.getenv("PG_PASSWORD")),
-        database = os.getenv("PG_DATABASE"),
-        host = os.getenv("PG_HOST"),
-        port = int(os.getenv("PG_PORT"))
+        user=os.getenv("PG_USER"),
+        password=str(os.getenv("PG_PASSWORD")),
+        database=os.getenv("PG_DATABASE"),
+        host=os.getenv("PG_HOST"),
+        port=int(os.getenv("PG_PORT")),
     )
+
 
 def close_connection(db):
     db.close()
-    
-
--- test/test_pg_connection.py
+++ test/test_pg_connection.py
@@ -1,16 +1,14 @@
-import os
-
-from dotenv import load_dotenv
-from unittest.mock import patch
 import pytest
 
 from src.util.pg_connection import connect_to_db, close_connection
 
+
 def test_connect_to_db_connects():
     assert connect_to_db()
 
+
 def test_connect_to_db_fails_with_wrong_details(monkeypatch):
-    monkeypatch.setenv('PG_USER','error')
+    monkeypatch.setenv("PG_USER", "error")
     db = None
     try:
         with pytest.raises(Exception):
@@ -19,10 +17,11 @@ def test_connect_to_db_fails_with_wrong_details(monkeypatch):
         if db:
             close_connection(db)
 
+
 def test_close_connection():
     db = connect_to_db()
     result = db.run("SELECT * FROM STAFF")
     assert isinstance(result, list)
     close_connection(db)
     with pytest.raises(Exception):
-        result_2 = db.run("SELECT * FROM STAFF")
+        db.run("SELECT * FROM STAFF")
