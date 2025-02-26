6c2acd23c8011d07bcf095e0c8f86826408b7390
AndrewFudge <fudge@gmx.co.uk>
2025-02-26 16:03:50 +0000

    updated ingress lambda for pep8:

--- src/ingestion_lambda.py
+++ src/ingestion_lambda.py
@@ -1,9 +1,7 @@
-from src.util.ingress import ingress_handler
+# from src.util.ingress import ingress_handler
 
 def ingestion_lambda_handler(event, context):
     pass
     # Retrieve all data into data dumps list
     # ingress_handler
     # export data into s3 bucket
-    
-    
