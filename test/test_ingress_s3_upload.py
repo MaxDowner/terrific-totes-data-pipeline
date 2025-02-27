947c079457efea36f7460d1058829ce8a27e9e5f
AndrewFudge <fudge@gmx.co.uk>
2025-02-27 17:14:05 +0000

    replaced test_ingress_s3_upload with older version as files were messed up in a merge?

--- test/test_ingress_s3_upload.py
+++ test/test_ingress_s3_upload.py
@@ -18,43 +18,74 @@ def aws_credentials():
 
 def test_upload_function_that_the_bucket_exists():
     with mock_aws():
-        test_client = boto3.client('s3')
-        bucket_name = 'test_bucket1'
+        test_client = boto3.client("s3")
+        bucket_name = "test_bucket1"
+        object_key = "test/uploaded_object"
+        dummy_object = "{name: hello}"
+
+        # Create a mock Bucket
+        test_client.create_bucket(
+            Bucket=bucket_name,
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
+        # Upload a file to the bucket
+        upload_ingestion_to_s3(
+            test_client, bucket_name, object_key, dummy_object
+        )
         listed_buckets = test_client.list_buckets()
-        assert listed_buckets['Buckets'][0]['Name'] == bucket_name
+
+        assert listed_buckets["Buckets"][0]["Name"] == bucket_name
 
 
 def test_upload_function_writes_the_object_to_the_bucket():
     with mock_aws():
-        test_client = boto3.client('s3')
-        bucket_name = 'test_bucket1'
-        object_key = 'test/uploaded_object'
-        listed_objects = test_client.list_objects_v2(
-            Bucket=bucket_name
+        test_client = boto3.client("s3")
+        bucket_name = "test_bucket1"
+        object_key = "test/uploaded_object"
+        dummy_object = "{name: hello}"
+
+        # Create a mock Bucket
+        test_client.create_bucket(
+            Bucket=bucket_name,
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
         )
+
+        # Upload a file to the bucket
+        upload_ingestion_to_s3(
+            test_client, bucket_name, object_key, dummy_object
+        )
+
+        listed_objects = test_client.list_objects_v2(Bucket=bucket_name)
         # Retrieve Object contents
         get_my_object = test_client.get_object(
-            Bucket=bucket_name,
-            Key=object_key
-            )
-        object_content = get_my_object['Body'].read().decode('utf-8')
+            Bucket=bucket_name, Key=object_key
+        )
+        object_content = get_my_object["Body"].read().decode("utf-8")
+
         # Assert
-        assert listed_objects['Contents'][0]['Key'] == object_key
-        assert object_content == '{name: hello}'
+        assert listed_objects["Contents"][0]["Key"] == object_key
+        assert object_content == "{name: hello}"
 
 
 def test_upload_function_raises_exception_if_bucket_not_present():
     with mock_aws():
         # setup client
-        test_client = boto3.client('s3')
-        object_key = 'test/uploaded_object'
-        dummy_object = '{name: hello}'
-        fake_bucket_name = 'fake_bucket'
+        test_client = boto3.client("s3")
+        bucket_name = "test_bucket1"
+        object_key = "test/uploaded_object"
+        dummy_object = "{name: hello}"
+        fake_bucket_name = "fake_bucket"
+
+        # create real bucket
+        test_client.create_bucket(
+            Bucket=bucket_name,
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
+
         # try to upload a file to a fake bucket
         with pytest.raises(ClientError) as exc:
-            upload_ingestion_to_s3(test_client,
-                                   fake_bucket_name,
-                                   object_key,
-                                   dummy_object)
+            upload_ingestion_to_s3(
+                test_client, fake_bucket_name, object_key, dummy_object
+            )
         err = exc.value.response["Error"]
-        assert err["Message"] == 'The specified bucket does not exist'
+        assert err["Message"] == "The specified bucket does not exist"
