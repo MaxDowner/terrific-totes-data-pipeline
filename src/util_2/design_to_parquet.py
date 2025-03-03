b4ac8ac47936eb586104beea0e333da166e62f9d
Morgan Lamb <morgan.lamb@hotmail.com>
2025-03-03 16:09:20 +0000

    Minor updates

--- src/util_2/design_to_parquet.py
+++ src/util_2/design_to_parquet.py
@@ -11,11 +11,11 @@ def design_to_parquet(updated_rows: list):
     # each element in updated_rows is an updated row: dict
     output_dict = {"dim_design": updated_rows}
 
-    json_format = json.dumps(output_dict)
-    print(json_format)
-    table = pj.read_json(json_format.body()) 
-    print(table)
-    pq.write_table(table, 'test_results.parquet')
+    with open("tmp/output_dict.json", 'w') as f:
+        json.dump(output_dict, f)
+
+    table = pj.read_json("tmp/output_dict.json") 
+    pq.write_table(table, 'tmp/formatted_dim_designs.parquet')
 
 # output -> "dim_design": [
     # {"design_id": data_1, "design_name": data_2, "file_location": data_3, "file_name" : data_4}
