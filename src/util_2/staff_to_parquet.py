d4f5ae7123115fc9eba9fb996e6779140f912e04
AndrewFudge <fudge@gmx.co.uk>
2025-03-04 08:45:05 +0000

    updated to add doc string and cleaner formatting

--- src/util_2/staff_to_parquet.py
+++ src/util_2/staff_to_parquet.py
@@ -3,20 +3,17 @@ import json
 import pyarrow.parquet as pq
 
 def process_staff(updated_rows: list):
+    """takes a list of updated data for staff
+    converts the data to parquet
+
+    Args:
+        updated_rows (list): list of updated staff data
+    """
     # each element in updated_rows is an updated row: dict
-    output_dict = {"staff": updated_rows}
-    print(updated_rows)
+    # output_dict = {"staff": updated_rows}
     with open("/tmp/output_staff_dict.json", 'w') as f:
-        # json.dump(output_dict, f)
-        json_str = ""
-        # f.writelines(updated_rows)
         for dict in updated_rows:
             line = json.dumps(dict)
             f.write(line + '\n')
-            # json_str += str(dict) + '\n'
-        print(json_str)
-        # # json.dump(json_str, f)
-
-
     table = pj.read_json("/tmp/output_staff_dict.json") 
     pq.write_table(table, '/tmp/formatted_dim_staff.parquet')
