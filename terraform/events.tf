853fbe79405120ebc95511667b7541e6ecfdc471
Georgina <georgina.hardcastle@gmail.com>
2025-03-04 12:30:34 +0000

    added partial terraform for transform phase, work in progress

--- terraform/events.tf
+++ terraform/events.tf
@@ -1,5 +1,4 @@
 resource "aws_cloudwatch_event_rule" "scheduler" {
-  # timings need changing to 15 min for actual lambda func!!!!!
   name                = "lambda_scheduler"
   description         = "triggers lambda func every 15 minutes"
   schedule_expression = "rate(15 minutes)"
