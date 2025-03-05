de82edea5454d013f3aca33b73e87d84e26598bf
Georgina <georgina.hardcastle@gmail.com>
2025-03-05 16:57:06 +0000

    most of terraform phase 2 complete; processing data lambda throwing error

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -12,8 +12,6 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.processing_lambda_handler_resource.arn
     events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "AWSLogs/"
-    filter_suffix       = ".log"
   }
 
   depends_on = [aws_lambda_permission.allow_bucket]
