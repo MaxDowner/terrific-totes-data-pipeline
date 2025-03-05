f1299da56c11107b0f437b5a5ec7104d2e9bcb32
Georgina <georgina.hardcastle@gmail.com>
2025-03-05 11:08:50 +0000

    add eventbridge resources for s3 trigger

--- terraform/events_2.tf
+++ terraform/events_2.tf
@@ -1,1 +1,9 @@
-## 
+resource "aws_cloudwatch_event_rule" "s3_event_rule" {
+  name                = "processing_lambda_s3_rule"
+  description         = "executes lambda on s3 update"
+  event_pattern       = jsonencode({"source": ["aws.s3"],
+                        "detail-type": ["Object Created"]})
+}
+
+
+
