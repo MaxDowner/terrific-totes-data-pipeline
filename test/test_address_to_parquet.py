76b44fbc3d1543dda0bab42813da49cf413d6207
CharlotteHooson <CharlotteHooson@outlook.com>
2025-03-05 10:36:53 +0000

    complete address to parquet util func and tests. Also update json to have accurate address data

--- test/test_address_to_parquet.py
+++ test/test_address_to_parquet.py
@@ -1,56 +1,59 @@
-# import os
-# from src.util_2.address_to_parquet import address_to_parquet
-# import pyarrow.parquet as pq
+import os
+from src.util_2.address_to_parquet import address_to_parquet
+import pyarrow.parquet as pq
 
-# staff_dict = {
-#     "staff": [
-#         {
-#             "staff_id": 1,
-#             "first_name": "Jeremie",
-#             "last_name": "Franey",
-#             "department_name": "Purchasing",
-#             "location": "Manchester",
-#             "email_address": "jeremie.franey@terrifictotes.com",
-#         },
-#         {
-#             "staff_id": 2,
-#             "first_name": "Deron",
-#             "last_name": "Beier",
-#             "department_name": "Facilities",
-#             "location": "Manchester",
-#             "email_address": "deron.beier@terrifictotes.com",
-#         },
-#         {
-#             "staff_id": 3,
-#             "first_name": "Jeanette",
-#             "last_name": "Erdman",
-#             "department_name": "Facilities",
-#             "location": "Manchester",
-#             "email_address": "jeanette.erdman@terrifictotes.com",
-#         },
-#     ]
-# }
+address_dict = {
+    "address": [
+        {
+            "address_id": 1,
+            "address_line_1": "9 Bogart Hill",
+            "address_line_2": "Avon",
+            "district": "Avon",
+            "city": "Patienceburgh",
+            "postal_code": "WM60 3FH",
+            "country": "Turkey",
+        },
+        {
+            "address_id": 2,
+            "address_line_1": "12 Street Lane",
+            "address_line_2": None,
+            "district": "Aliso Viejo",
+            "city": "Aliso Viejo",
+            "postal_code": "ED6 12PY",
+            "country": "San Marino",
+        },
+        {
+            "address_id": 30,
+            "address_line_1": "12 Alaska Place",
+            "address_line_2": "The Avenue",
+            "district": "Buckinghamshire",
+            "city": "Morton",
+            "postal_code": "NG9 8IP",
+            "country": "Falkland Islands (Malvinas)"
+        }
+    ]
+}
 
-# staff_list = staff_dict["staff"]
-
-
-# def test_process_staff_returns_a_pq_file():
-#     if os.path.exists("/tmp/formatted_dim_staff.parquet"):
-#         os.remove("/tmp/formatted_dim_staff.parquet")
-#     process_staff(staff_list)
-#     assert os.path.exists("/tmp/formatted_dim_staff.parquet")
+address_list = address_dict["address"]
 
 
-# def test_pq_file_is_readable():
-#     if os.path.exists("/tmp/formatted_dim_staff.parquet"):
-#         os.remove("/tmp/formatted_dim_staff.parquet")
-#     process_staff(staff_list)
-#     # with open("/tmp/formatted_dim_staff.parquet", 'r') as f:
-#     #     pass
-#     table = pq.read_table("/tmp/formatted_dim_staff.parquet")
-#     # parquet_file = pq.ParquetFile("/tmp/formatted_dim_staff.parquet")
-#     metadata = pq.read_metadata("/tmp/formatted_dim_staff.parquet")
-#     assert str(table["first_name"][0]) == "Jeremie"
-#     assert str(table["department_name"][2]) == "Facilities"
-#     assert metadata.num_columns == 6
-#     assert metadata.num_rows == 3
+def test_process_address_returns_a_pq_file():
+    if os.path.exists("/tmp/formatted_dim_address.parquet"):
+        os.remove("/tmp/formatted_dim_address.parquet")
+    address_to_parquet(address_list)
+    assert os.path.exists("/tmp/formatted_dim_address.parquet")
+
+
+def test_pq_file_is_readable():
+    if os.path.exists("/tmp/formatted_dim_address.parquet"):
+        os.remove("/tmp/formatted_dim_address.parquet")
+    address_to_parquet(address_list)
+    # with open("/tmp/formatted_dim_address.parquet", 'r') as f:
+    #     pass
+    table = pq.read_table("/tmp/formatted_dim_address.parquet")
+    # parquet_file = pq.ParquetFile("/tmp/formatted_dim_address.parquet")
+    metadata = pq.read_metadata("/tmp/formatted_dim_address.parquet")
+    assert str(table["address_line_1"][0]) == "9 Bogart Hill"
+    assert str(table["city"][2]) == "Morton"
+    assert metadata.num_columns == 7
+    assert metadata.num_rows == 3
