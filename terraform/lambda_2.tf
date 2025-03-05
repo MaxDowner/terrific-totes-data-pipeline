267410e7911f93e193830189ea083b07de39e10b
AndrewFudge <fudge@gmx.co.uk>
2025-03-05 19:16:23 +0000

    added util 1 as layer and aws managed pandas as layer as it is big and needed

--- terraform/lambda_2.tf
+++ terraform/lambda_2.tf
@@ -18,7 +18,7 @@ resource "aws_lambda_function" "processing_lambda_handler_resource" {
   handler          = "processing_lambda.processing_lambda_handler"
   timeout          = 200
   source_code_hash = data.archive_file.lambda_2.output_base64sha256
-  layers = [aws_lambda_layer_version.dependencies.arn, aws_lambda_layer_version.util_layer_2.arn]
+  layers = [aws_lambda_layer_version.dependencies.arn, aws_lambda_layer_version.util_layer.arn, aws_lambda_layer_version.util_layer_2.arn, "arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python312:16"]
   depends_on = [
     aws_iam_role_policy_attachment.lambda_2_cw_policy_attachment,
     aws_cloudwatch_log_group.processing_group,
