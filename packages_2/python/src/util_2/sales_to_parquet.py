6617ab07a305fe34ce63601984207fc929c99123
AndrewFudge <fudge@gmx.co.uk>
2025-03-05 19:19:50 +0000

    updated with the pa.field stuff

--- packages_2/python/src/util_2/sales_to_parquet.py
+++ packages_2/python/src/util_2/sales_to_parquet.py
@@ -1,6 +1,7 @@
 from pyarrow import json as pj
 import json
 import pyarrow.parquet as pq
+import pyarrow as pa
 
 
 # {
@@ -29,10 +30,46 @@ def sales_to_parquet(updated_rows: list):
     Args:
         updated_rows (list): list of updated sales data
     """
-
+    # Schema and casting is to remove time from date
+    raw_schema = pa.schema([
+        pa.field('sales_order_id', pa.int64()),
+        pa.field('created_date', pa.string()),
+        pa.field('created_time', pa.string()),
+        pa.field('last_updated_date', pa.string()),
+        pa.field('last_updated_time', pa.string()),
+        pa.field('staff_id', pa.int64()),
+        pa.field('counterparty_id', pa.int64()),
+        pa.field('units_sold', pa.int64()),
+        pa.field('unit_price', pa.string()),
+        pa.field('currency_id', pa.int64()),
+        pa.field('design_id', pa.int64()),
+        pa.field('agreed_payment_date', pa.string()),
+        pa.field('agreed_delivery_date', pa.string()),
+        pa.field('agreed_delivery_location_id', pa.int64())
+    ])
+    processed_schema = pa.schema([
+        pa.field('sales_order_id', pa.int64()),
+        pa.field('created_date', pa.date32()),
+        pa.field('created_time', pa.string()),
+        pa.field('last_updated_date', pa.date32()),
+        pa.field('last_updated_time', pa.string()),
+        pa.field('staff_id', pa.int64()),
+        pa.field('counterparty_id', pa.int64()),
+        pa.field('units_sold', pa.int64()),
+        pa.field('unit_price', pa.float64()),
+        pa.field('currency_id', pa.int64()),
+        pa.field('design_id', pa.int64()),
+        pa.field('agreed_payment_date', pa.date32()),
+        pa.field('agreed_delivery_date', pa.date32()),
+        pa.field('agreed_delivery_location_id', pa.int64())
+    ])
     with open("/tmp/output_sales_dict.json", "w") as f:
         for dict in updated_rows:
             line = json.dumps(dict)
             f.write(line + "\n")
-    table = pj.read_json("/tmp/output_sales_dict.json")
+    raw_table = pj.read_json(
+        "/tmp/output_sales_dict.json",
+        parse_options=pj.ParseOptions(explicit_schema=raw_schema)
+        )
+    table = raw_table.cast(processed_schema)
     pq.write_table(table, "/tmp/formatted_fact_sales.parquet")
