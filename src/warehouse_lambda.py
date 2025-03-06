0bcac3c1f4535f300c92127ab18256ec0e88942e
Morgan Lamb <morgan.lamb@hotmail.com>
2025-03-06 15:31:29 +0000

    update warehouse_lambda and associated tests with all tests passing

--- src/warehouse_lambda.py
+++ src/warehouse_lambda.py
@@ -1,40 +1,40 @@
 # imports
+import boto3
 
 #function:
 def warehouse_lambda_handler(event, context):
-    processing_bucket = event["Records"][0]["s3"]["bucket"]["name"]
-    return processing_bucket
+    processing_bucket = event["Records"][0]["s3"]["bucket"]["name"] # <<< Bucket to get data from
+    parquet_download_key = event["Records"][0]["s3"]["object"]["key"]  # <<< Key to access data
+    table_name = parquet_download_key[29:-8]        # << table to update
+
+    s3_client = boto3.client("s3")
+    filepath = '/tmp/downloaded_file.parquet'
+    try:
+        s3_client.download_file(
+            processing_bucket, parquet_download_key, filepath
+        )
+    except:
+        pass
+
+    return [processing_bucket, parquet_download_key, table_name]
+
+
+
 """
-This lambda will be invoked by any updates to PROCESSED-DATA bucket
+# Download the parquet file
+s3_client.download_file(
+    processing_bucket, parquet_download_key, parquet_file_name
+)
+logger.info(f'Downloaded data to "{parquet_file_name}" inside Lambda')
 
-# BUCKET THAT CREATES EVENT
-# print(processing_bucket)
-# (HOPEFULLY) LIST OF KEYS OF OBJECTS THAT ARE NEW IN BUCKET
-parquet_download_key = event["Records"][0]["s3"]["object"]["key"] < MULTIPLE PARQUET FILES PER INVOKATION???
-# print(parquet_download_key)
-# MAY BE IRRELEVANT
-parquet_file_name = f"/tmp/{parquet_download_key[-10:]}" < CHANGE THIS to START:END OF TIME KEY
-# print(parquet_file_name)
+use local (tmp) parquet file in query
 
-s3_client = boto3.client("s3")
+# query = fUPDATE {table_name}  < USING PARQUET SQL SHIZZLE
 
-Gets those objects from bucket
-# ITERATE THROUGH LIST OF OBJECT KEYS
-
-    # Download the parquet file
-    s3_client.download_file(
-        processing_bucket, parquet_download_key, parquet_file_name
-    )
-    logger.info(f'Downloaded data to "{parquet_file_name}" inside Lambda')
-
-    # DO SLICING ON OBJECT KEY TO FIND DESTINATION TABLE
-    # target_table = key[x:y]  # dim_design
-    # query = fUPDATE {target_table}  < USING PARQUET SQL SHIZZLE
-    
-    get secrets for data warehouse
-    connect to db using warehouse secrets
-    execute query
-    close db
-    log yay
+get secrets for data warehouse
+connect to db using warehouse secrets
+execute query
+close db
+log yay
 
 """
--- test/test_warehouse_lambda.py
+++ test/test_warehouse_lambda.py
@@ -1,18 +1,91 @@
 from src.warehouse_lambda import warehouse_lambda_handler
+import pytest
+import os
+import boto3
+from moto import mock_aws
 
-def test_warehouse_lambda_gets_correct_bucket():
+@pytest.fixture(scope="function")
+def aws_credentials():  # credentials required for testing
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+event = {
+    "Records": [
+        {
+            "eventVersion": "2.0",
+            "eventSource": "aws:s3",
+            "awsRegion": "eu-west-2",
+            "eventTime": "1970-01-01T00:00:00.000Z",
+            "eventName": "ObjectCreated:Put",
+            "userIdentity": {"principalId": "EXAMPLE"},
+            "requestParameters": {"sourceIPAddress": "127.0.0.1"},
+            "responseElements": {
+                "x-amz-request-id": "EXAMPLE123456789",
+                "x-amz-id-2": "EXAMPLE123/5678abcdefghijklambdaisawesome",
+            },
+            "s3": {
+                "s3SchemaVersion": "1.0",
+                "configurationId": "testConfigRule",
+                "bucket": {
+                    "name": "processed-data",
+                    "ownerIdentity": {"principalId": "EXAMPLE"},
+                    "arn": "arn:aws:s3:::ingested-data123456",
+                },
+                "object": {
+                    "key": "2025/03/03/14/37-14formatted_dim_address.parquet",
+                    "size": 1024,
+                    "eTag": "0123456789abcdef0123456789abcdef",
+                    "sequencer": "0A1B2C3D4E5F678901",
+                },
+            },
+        }
+    ]
+}
+
+def test_warehouse_lambda_gets_correct_bucket(aws_credentials):
     # Arrange
-    event = {
-            'Records': [
-                {'s3': 
-                    {'bucket':
-                        {'name': "processed-data"}
-                    }
-                }
-            ]
-    }
-    expected = "processed-data"
-    # Act
-    result = warehouse_lambda_handler(event, {})
-    # Assert
-    assert expected == result
+    with mock_aws():
+        expected = "processed-data"
+        # Act
+        result = warehouse_lambda_handler(event, {})
+        # Assert
+        assert expected == result[0]
+
+def test_warehouse_lambda_gets_correct_key(aws_credentials):
+    # Arrange
+    with mock_aws():
+        expected = "2025/03/03/14/37-14formatted_dim_address.parquet"
+        # Act
+        result = warehouse_lambda_handler(event, {})
+        # Assert
+        assert expected == result[1]
+
+def test_warehouse_lambda_gets_correct_table_name(aws_credentials):
+    # Arrange
+    with mock_aws():
+        expected = "dim_address"
+        # Act
+        result = warehouse_lambda_handler(event, {})
+        # Assert
+        assert expected == result[2]
+
+def test_warehouse_lambda_downloads_file(aws_credentials):#
+    # Arrange
+    if os.path.exists("/tmp/downloaded_file.parquet"):
+        os.remove("/tmp/downloaded_file.parquet")
+    with mock_aws():
+        test_client = boto3.client('s3')
+        test_client.create_bucket(
+            Bucket='processed-data',
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"}
+        )
+        file = 'test_files/44-50formatted_dim_currency.parquet'
+        object_key = '2025/03/03/14/37-14formatted_dim_address.parquet'
+        # Act
+        test_client.upload_file(file, 'processed-data', object_key)
+        result = warehouse_lambda_handler(event, {})
+        # Assert
+        assert os.path.exists("/tmp/downloaded_file.parquet")
