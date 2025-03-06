5f2856fa72e7d2dff6b200f49e223ca93ac1d507
Hamzah Saeid <hamzahsaeid@gmail.com>
2025-03-06 15:29:37 +0000

    load to dw first iteration

---
+++ src/util_3/load_to_warehouse.py
@@ -0,0 +1,98 @@
+import logging
+# import pg8000
+import pandas as pd
+import pyarrow
+import pyarrow.dataset
+import pyarrow.parquet as pq
+import boto3
+import adbc_driver_postgresql.dbapi as adbc
+from src.util.get_secret import get_secret
+
+table_list = ["currency",
+              "staff",
+              "design",
+              "address",
+              "counterparty",
+              "sales_order"]
+column_list = [
+    ["currency_id", "currency_code"],
+    [
+        "staff_id",
+        "first_name",
+        "last_name",
+        "department_name",
+        "location",
+        "email_address",
+    ],
+    ["design_id", "design_name", "file_location", "file_name"],
+    [
+        "address_id",
+        "address_line_1",
+        "address_line_2",
+        "district",
+        "city",
+        "postal_code",
+        "country",
+        "phone",
+    ],
+    [
+        "counterparty_id",
+        "counterparty_legal_name",
+        "address_line_1",
+        "address_line_2",
+        "district",
+        "city",
+        "postal_code",
+        "country",
+        "phone",
+    ],
+    [
+        "sales_order_id",
+        "staff_id",
+        "counterparty_id",
+        "units_sold",
+        "unit_price",
+        "currency_id",
+        "design_id",
+        "agreed_delivery_date",
+        "agreed_payment_date",
+        "agreed_delivery_location_id",
+    ],
+]
+
+"""
+Need to get the secret
+"""
+#  uri = "postgresql://postgres:password@localhost:5432/postgres"
+
+def load_to_dw(secret, file, table_name):
+    uri = f"postgresql://{secret['username']}:{secret['password']}@{secret['host']}"\
+    f":{secret['port']}/{secret['dbname']}"
+    print(uri) 
+
+    conn = adbc.connect(uri)
+
+    print(conn.adbc_get_table_schema(table_name)) 
+    
+    with conn.cursor() as cur:
+        # reader = pq.ParquetFile(file)
+        # print(reader)
+        # cur.adbc_ingest(table_name, reader.iter_batches(), mode="create")
+
+        # reader = pyarrow.dataset.dataset(
+
+        # )
+        table = pq.read_table(file)
+        print(table)
+
+
+if __name__ == '__main__':
+    client = boto3.client("secretsmanager")
+    db_details = get_secret(client, 'totes-data-warehouse')
+    table = 'dim_staff'
+    file = 'test_files/58-58formatted_fact_sales.parquet'
+    load_to_dw(db_details, file, table)       
+     
+
+
+
---
+++ test/test_load_to_dw.py
@@ -0,0 +1,10 @@
+from src.util_3.load_to_warehouse import load_to_dw
+from moto import mock_aws, secretsmanager
+import boto3
+import pytest
+import os
+from unittest.mock import patch
+
+
+def test1():
+    pass
