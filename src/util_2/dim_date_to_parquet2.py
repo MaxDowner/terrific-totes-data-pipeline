aa1857ce9cd463621f423a60761a83f5920a4e10
AndrewFudge <fudge@gmx.co.uk>
2025-03-12 12:06:57 +0000

    added doc string and refactored code

--- src/util_2/dim_date_to_parquet2.py
+++ src/util_2/dim_date_to_parquet2.py
@@ -2,12 +2,11 @@ import pandas as pd
 
 
 def create_dataset():
-    """
-    returns a dataframe
+    """Generate a dim_date table in parquet 
+    and write to a file in the /tmp/ folder.
     """
     # creates dataset
     df = pd.DataFrame({"date_id": pd.date_range("2022-01-01", "2030-12-31")})
-    # df['date_id'] = df['date_id'].dt.date
     df["year"] = df.date_id.dt.year
     df["month"] = df.date_id.dt.month
     df["day"] = df.date_id.dt.day_of_year
@@ -17,8 +16,4 @@ def create_dataset():
     df["quarter"] = df.date_id.dt.quarter
     df['date_id'] = df.date_id.dt.date
 
-    print(df)
     df.to_parquet("/tmp/formatted_dim_date.parquet")
-
-# if __name__ == '__main__':
-#     create_dataset()
